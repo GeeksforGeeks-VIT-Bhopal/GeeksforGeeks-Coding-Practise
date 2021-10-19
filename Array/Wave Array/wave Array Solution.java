051a9edc2fc346466e8f3ee8b646347c8ea0d5b2
shantam sultania <48251619+shantamsultania@users.noreply.github.com>
2021-10-19 13:24:53 +0530

    Update wave Array Solution.java

--- Array/Wave Array/wave Array Solution.java
+++ Array/Wave Array/wave Array Solution.java
@@ -1,16 +1,27 @@
+import java.util.*;
+
 public class Main
 {
   public static void main (String[]args)
   {
 
     Solution3 object = new Solution3();
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("enter the limit of the array");
+    int n = scanner.nextInt();
     
+    System.out.println("Enter the array Elements");
+    
+    int arr[] = new int[n];
+    
+    for(int i=0;i<n;i++){
+      arr[i] = scanner.nextInt();
+    }
 
-    int arr[] = {1,2,3,4,5};
-    int n = arr.length;
     
     int result[] = object.toWave(arr,n);
 
+    System.out.println("the result is : ");
     for (int i=0;i<n ;i++ ){
         System.out.print(result[i]);
     } 
